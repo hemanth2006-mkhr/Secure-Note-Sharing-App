ba0a8bfe6a6b9fef39c3ade96b56a2123551f308
hemanth-ubuntu <hemanth.m1102@gmail.com>
2026-07-16 20:44:52 +0530

    Update API base URL to production endpoint

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1"
+    baseURL: "https://secure-note-sharing-app.onrender.com"
 });
 
 // Automatically attach JWT
